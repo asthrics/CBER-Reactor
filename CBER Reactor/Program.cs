2f1b82f0622fc1c233df71ac4ca939a75417678b
asthrics <sebastianlercher@icloud.com>
2026-05-27 09:03:18 +0200

    Created Repo
    Had to do some manual File Structure Changing??
    Weird OneDrive

---
+++ CBER Reactor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CBER_Reactor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Test!");
+        }
+    }
+}
